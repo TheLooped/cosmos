0f144b2cea0694152412a4fe716a870eb5a51249
TheLooped <95268961+TheLooped@users.noreply.github.com>
2024-02-17 20:48:11 -0500

    Update Palette

--- lua/cosmos/palette.lua
+++ lua/cosmos/palette.lua
@@ -1,43 +1,52 @@
 local palette = {
-    cosmic_void  = "#070714",
-    starry_night = "#131324",
-    cosmic_abyss = "#06030d",
-    lunar_dust   = "#161626",
-    dusty_crater = "#121221",
+    cosmic_void  = "#100c29", -- Main BG
+    cosmic_abyss = "#17132f", -- Alt BG
+
+    dusty_crater = "#1b1732", -- BG Highlight hard
+    starry_night = "#1d1934", -- BG Highlight medium
+    lunar_dust   = "#1e1a35", -- BG Highlight soft
 
 
-    starlight = "#D5E0EC",
-    stardust  = "#ABC0D8",
-    moonshine = "#C7E1FB",
-    moonlight = "#b4b1fc",
+    starlight = "#D5E0EC", -- Main FG
+    moonshine = "#C7E1FB", -- Alt FG
+
+    moonlight = "#A6B3E3",
+    stardust  = "#AEBDE1",
+    celestial = "#c7d3ea",
 
 
-    nebula      = "#657CE2",
+
+    nebula      = "#657CE2", -- Keyword
     sky         = "#8799E8",
     mist        = "#8f99cc",
 
+    celeste     = "#A1F6FF", -- Builtin
+    cygnus      = "#B2FBFF",
     seafoam     = "#C2FFFF",
-    celeste     = "#86EEFE",
 
-    moss        = "#4CE47E",
+    moss        = "#4CE47E", -- Constant
     floss       = "#74D899",
     mint_leaf   = "#9AE4A6",
 
-    solar_flare = "#FF7733",
+    solar_flare = "#FF7733", -- Variable
     nova        = "#FF8547",
+    sunset      = "#FFB96D",
 
+    lilac       = "#FF9AEE", -- Special
+    cotton      = "#FFA4F0",
     rose        = "#FFADF1",
-    lilac       = "#FF9AEE",
 
-    amethyst    = "#9758D6",
-    wisteria    = "#7d53c2",
+    wisteria    = "#8455c7", -- Function Names
+    lone        = "#8b56cc",
+    amethyst    = "#9157D1",
 
-    ember       = "#F22C2C",
+    ember       = "#F94444", -- Errors
     blaze       = "#FF5C5C",
     velvet      = "#f46363",
 
 
-    amber   = "#FFCC33",
-    sunglow = "#FFD147",
+    sunshine = "#FFCC33",    -- Type
+    sunglow  = "#FFD147",
+    sunset   = "#FFBE69",    -- Warnings
 }
 return palette
