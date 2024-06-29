6e02063002eb4bc54ac9fa3ba5eb6bff38d784eb
TheLooped <95268961+TheLooped@users.noreply.github.com>
2024-06-28 22:16:09 -0500

    Reimplementing palette

--- lua/cosmos/palette.lua
+++ lua/cosmos/palette.lua
@@ -1,51 +1,42 @@
 local palette = {
-	cosmic_void = "#1C1F33", -- Main BG
-	cosmic_abyss = "#171726", -- Alt BG
-	wormhole = "#2F2F4C",
 
-	starry_night = "#2A2D41", -- BG Highlight hard
-	lunar_dust = "#2e3145", -- BG Highlight medium
-	dusty_crater = "#303347", -- BG Highlight soft
+    -----------------
+    ---BackGrounds---
+    -----------------
 
-	starlight = "#D5E0EC", -- Main FG
-	moonshine = "#C7E1FB", -- Alt FG
+    deep_nebula = "#011423",
+    light_nebula = "#01192d",
+    cosmic_void = "#1C1F33",  -- Main BG
+    cosmic_abyss = "#171726", -- Alt BG
+    wormhole = "#2F2F4C",
 
-	moonlight = "#A6B3E3",
-	stardust = "#AEBDE1", -- Comments
-	stellar = "#AEC4D3",
-	celestial = "#c7d3ea",
+    starry_night = "#2A2D41", -- BG Highlight hard
+    lunar_dust = "#2e3145",   -- BG Highlight medium
+    dusty_crater = "#303347", -- BG Highlight soft
+    -- Main editor background
+    -- Sidebar background
+    -- Popup menu background
+    -- Selected item background
+    -- Diff background (for git changes)
 
-	nebula = "#657CE2", -- Keyword
-	sky = "#8799E8", -- Conditional
-	mist = "#8f99cc",
+    -----------------
+    ---ForeGrounds---
+    -----------------
 
-	celeste = "#A1F6FF", -- Builtin
-	cygnus = "#B2FBFF", -- Identifier
-	seafoam = "#C2FFFF",
-
-	moss = "#4CE47E", -- Constant
-	floss = "#74D899", -- Type
-	mint_leaf = "#9AE4A6",
-
-	solar_flare = "#FF7733", -- Variable
-	nova = "#FF8547", -- String
-	sunrise = "#FFB96D",
-
-	lilac = "#FF9AEE", -- Special
-	cotton = "#FFA4F0",
-	rose = "#FFADF1",
-
-	wisteria = "#8455c7", -- Function Names
-	lone = "#8b56cc",
-	amethyst = "#9157D1",
-	quartz = "#B39AD3",
-
-	ember = "#cc0202", -- Errors
-	blaze = "#FF5C5C", -- Secondary Errors
-	velvet = "#f46363",
-
-	sunshine = "#FFCC33", -- Type
-	sunglow = "#FFD147",
-	sunset = "#FFBE69", -- Warnings
+    starlight = "#D5E0EC", -- Main FG
+    moonshine = "#C7E1FB", -- Alt FG
+    moonlight = "#A6B3E3",
+    stardust = "#AEBDE1",  -- Comments
+    stellar = "#AEC4D3",
+    celestial = "#c7d3ea",
+    -- Normal text
+    -- Comments
+    -- Keywords
+    -- Strings
+    -- Numbers
+    -- Functions
+    -- Types/Classes
+    -- Variables
+    -- Constants
 }
 return palette
